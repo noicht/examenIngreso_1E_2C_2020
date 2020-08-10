83ae3a62906ada65339eeb37a2dc64e5bbf9f0ac
noicht <tincho_cabrera98@outlook.com>
2020-08-10 20:05:57 -0300

    subo ej 2

--- dos.js
+++ dos.js
@@ -7,5 +7,100 @@ b)la marca del más caro de los líquidos
 c)la marca del más barato de los sólidos
 */function mostrar()
 {
-  alert("dos");
+
+	let cantProductos = 0;
+	let tipoProd;
+	let marca;
+	let precio = 0;
+	let precioMax = 0;
+
+	let peso;
+	let pesoMasLiviano;
+
+	let pesoTotal = 0;
+
+	let marcaMasCara;
+	let marcaMasLiviana;
+
+	let flagCaro = 0;
+	let flagLiviano = 0;
+	let respuesta = "si";
+
+	do
+	{
+	
+		do 
+		{
+
+	
+			tipoProd = prompt("Ingrese tipo de producto (solido/liquido)");
+
+
+
+		} while(!(tipoProd == "solido" || tipoProd == "liquido"));
+
+		do 
+		{
+
+			precio = parseInt(prompt("Ingrese precio"));
+			marca = prompt("Ingrese marca")
+
+			if (flagCaro == 0 || precio > precioMax && tipoProd == "liquido") 
+			{
+			precioMax = precio;
+			marcaMasCara = marca;
+			flagCaro = 1;
+			}
+
+
+
+
+		} while(!(precio > 0))
+
+		do 
+		{
+
+			peso = parseFloat(prompt("Ingrese peso"));
+
+			pesoTotal = pesoTotal + peso;
+
+			if (flagLiviano == 0 || pesoMasLiviano < peso && tipoProd == "solido") 
+			{
+			pesoMasLiviano = peso;
+			marcaMasLiviana = marca;
+			flagLiviano = 1;
+
+
+			}	
+
+
+
+
+		} while(!(peso > 0));
+
+
+
+
+
+
+
+
+
+
+	respuesta = prompt("desea agregar mas? si/no");
+
+} while(respuesta == "si");
+
+
+document.write("Informe peso total: " + pesoTotal + "kg" + "<br>");
+document.write("Marca mas cara de liquidos: " + marcaMasCara + "<br>");
+document.write("Marca mas liviana de solidos: " + marcaMasLiviana + "<br>");
+
+
+
+
 }
+
+
+
+
